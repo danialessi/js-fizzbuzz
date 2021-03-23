2f1a97104aa62355209075f315415e09754d4796
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-23 16:37:01 +0100

    modificata situazione iniziale ciclo for

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@
 var numero;
 
 var multiplo3 = numero % 3 == 0;
+// console.log(multiplo3);
 
 var multiplo5 = numero % 5 == 0;
+// console.log(multiplo5);
 
 // stampo i numeri da 1 a 100 
-for (var i = 0; i <= 100; i++) {
+for (var i = 1; i <= 100; i++) {
     var numero = i;
     console.log(numero);
-
 
     if (numero = multiplo3) {
         console.log("Fizz")
