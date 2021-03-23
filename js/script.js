480432b8daf8169dff5599ff2a529e098ff6797c
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-23 16:16:45 +0100

    eseguo ciclo per stampare i numeri da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// multiplo di un numero
+var numero;
+
+var multiplo3 = numero % 3 == 0;
+
+var multiplo5 = numero % 5 == 0;
+
+// stampo i numeri da 1 a 100 
+for (var i = 0; i <= 100; i++) {
+    var numero = i;
+    console.log(numero);
+
+
+    if (numero = multiplo3) {
+        console.log("Fizz")
+    } else if (numero = multiplo5) {
+        console.log("Buzz")
+    }
+}
